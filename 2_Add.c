4940e5ac134026b2b648f3ad2b1f253c1bf1870c
Sahilmanni90 <2020a1r155@mietjammu.in>
2021-03-19 12:47:05 +0530

    Adding add to repository

---
+++ 2_Add.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int number1,number2,number3,sum;
+	scanf("%d %d %d", &number1,&number2,&number3);
+	sum=number1+number2+number3;
+	printf("\n Sum of Three Number is = %d",sum);
+	return 0;
+}
