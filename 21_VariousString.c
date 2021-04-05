237728cbea8c12c00eaba94b3f34418e6eafb8d2
Sahilmanni90 <2020a1r155@mietjammu.in>
2021-04-05 23:18:00 +0530

    Adding VariousString to repository

---
+++ 21_VariousString.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    /* String Declaration*/
+    char nickname[20];
+
+    printf("Enter your Nick name:");
+
+    scanf("%s", nickname);
+
+    /*Displaying String*/
+    printf("%s",nickname);
+
+    return 0;
+}
