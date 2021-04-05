d4908c7d7d5fe63e061cf2b6ddf803549cd44a87
Sahilmanni90 <2020a1r155@mietjammu.in>
2021-04-05 13:23:50 +0530

    Adding Factorial to add

---
+++ 09_AreaPerimeter.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	float raduisOfCircle, sideOfSquare, legthOfRectangle, breadthOfRectangle;
+	raduisOfCircle=7;
+	sideOfSquare=2;
+	legthOfRectangle=2;
+	breadthOfRectangle=2;
+	int %d
+	float %f
+	char %c
+	double %lf 
+	long int %ld
+	//scanf("%f %f %f %f",&radiusofCircle,&sideofSquare,&lengthofRectangle,&breadthofRectangle);
+	float areaOfCircle=((22*raduisOfCircle*raduisOfCircle)/7);
+	float perimeterOfCircle=(2*22*raduisOfCircle)/7;
+	printf("Area of Circle: %f",raduisOfCircle);
+	printf("Perimeter of Circle: %f",perimeterOfCircle);
+	printf("\nArea of Square: %f",(sideOfSquare*sideOfSquare));
+	printf("\nPerimeter of Square: %f",(4*sideOfSquare));
+	printf("\nArea of Rectangle: %f",(legthOfRectangle*breadthOfRectangle));
+	printf("\nPerimeter of Rectangle: %f",(2*(legthOfRectangle+breadthOfRectangle)));
+	return 0;
+}
