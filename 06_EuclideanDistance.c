5b1377cb1a92f02d27b72b26b3803ec409e229b0
sahilmanni90 <76391892+sahilmanni90@users.noreply.github.com>
2021-04-06 09:33:21 +0530

    Update 06_EuclideanDistance.c

--- 06_EuclideanDistance.c
+++ 06_EuclideanDistance.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
-#include<math.h>
+// C program to perform the Euclidean distance between two points
+// Author:Sahil Manni
+#include <stdio.h> // pre process directive to include standard input output header file
+#include<math.h> // pre process directive to include math header file
 void two_dis(float x1, float y1, float x2, float y2) {
    float dis = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2) * 1.0);
-   printf("Distance between 2 points are : %f", dis);
-   return;
+   printf("Distance between 2 points are : %f", dis); // Print the distance between two points
+   return 0;
 }
+// Start of main function body
 int main() {
    float x1 = 4;
    float y1 = 9;
