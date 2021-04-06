b41e17ce310695c1a06913047eff2c029edd2633
sahilmanni90 <76391892+sahilmanni90@users.noreply.github.com>
2021-04-06 13:06:25 +0530

    Update 07_EvenOdd.c

--- 07_EvenOdd.c
+++ 07_EvenOdd.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
-int main() {
+// A simple C program  to perform number is even or odd
+// Author:Sahil Manni
+#include <stdio.h> // pre process directive to include standard input output header file
+int main() // Start of the main function body
+{
+// Variable is to store the input number
     int num;
-    printf("Enter an integer: ");
-    scanf("%d", &num);
-    if(num % 2 == 0)
+    printf("Enter an integer: "); // Enter any integer
+    scanf("%d", &num); // Read the integer value
+ //  true if num is perfectly divisible by 2  
+    if(num % 2 == 0) 
         printf("%d is even.", num);
-    else
+    else 
         printf("%d is odd.", num);
     return 0;
 }
