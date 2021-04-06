a262c1cf1408df1cfc702030ee165e94a53e7b7d
sahilmanni90 <76391892+sahilmanni90@users.noreply.github.com>
2021-04-06 08:04:14 +0530

    Update 3_Hello.c

--- 3_Hello.c
+++ 3_Hello.c
@@ -1,6 +1,8 @@
-#include<stdio.h>
-int main()
+// Simple C program to display "Hello World"
+// Author:Sahil Manni
+#include<stdio.h> // Pre process directive to include standard input output header file
+int main()// start of the main function body
 {
-	printf("Hello World");
-	return 0;
+	printf("Hello World"); // prints hello world
+	return 0; // End of the body
 }
